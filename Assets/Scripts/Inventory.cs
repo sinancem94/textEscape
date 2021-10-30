5495cfcecc502781a522c51ff45b7bcdf7847959
cuneyturkan <72321020+cuneyturkan@users.noreply.github.com>
2021-10-30 17:18:28 +0300

    stat, inventory
    stat ve inventory ile ilgili şeyler eklendi.

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public inventory invent;
+
+    public void SetUp()
+    {
+        //ilk açılışta hepsi false, itemları topladıkça true'ya çekilecek.
+        invent.attickey = false;
+        invent.masterkey = false;
+        invent.rope = false;
+        invent.match = false;
+
+
+
+    }
+    public struct inventory
+    {
+        public bool attickey;
+        public bool masterkey;
+        public bool match;
+        public bool rope;
+
+    }
+}
